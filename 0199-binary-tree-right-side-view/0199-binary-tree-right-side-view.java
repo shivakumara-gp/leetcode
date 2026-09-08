dabcb20703f25dc734a76d1bd1451f97cf909826
shivakumara_gp <shivakumara7175@gmail.com>
2026-09-08 09:51:29 +0530

    1 ms (71.13%) 43.5 MB (69.29%) 09-08-2026 0199-binary-tree-right-side-view .java Medium

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,16 +13,16 @@
  *     }
  * }
  */
- class Tuple {
+//  class Tuple {
     
-    TreeNode node;
-    int hd;
+//     TreeNode node;
+//     int hd;
 
-    public Tuple(TreeNode _node, int _hd) {
-        node = _node;
-        hd = _hd;
-    }
-}
+//     public Tuple(TreeNode _node, int _hd) {
+//         node = _node;
+//         hd = _hd;
+//     }
+// }
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
          ArrayList<Integer> ans = new ArrayList<>();
@@ -31,36 +31,69 @@ class Solution {
             return ans;
         }
 
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        Queue<Tuple> q = new LinkedList<>();
+        
+        Queue<TreeNode> q = new LinkedList<>();
 
-        q.offer(new Tuple(root, 0));
+        q.offer(root);
 
         while (!q.isEmpty()) {
 
-            Tuple cur = q.poll();
-
-            TreeNode node = cur.node;
-            int hd = cur.hd;
-
-            
-            map.put(hd, node.val);
-
+           int length=q.size();
+            for(int i=0;i<length;i++){
+                
+            TreeNode node=q.poll();
+            if(i==length-1){
+                ans.add(node.val);
+            }
             if (node.left != null) {
-                q.offer(new Tuple(node.left, hd+1));
+                q.offer(node.left);
             }
 
             if (node.right != null) {
-                q.offer(new Tuple(node.right, hd + 1));
+                q.offer(node.right);
             }
         }
+           
+      }
 
-        for (int value : map.values()) {
-            ans.add(value);
-        }
 
         return ans;
     }
 }
+// ArrayList<Integer> ans = new ArrayList<>();
+
+//         if (root == null) {
+//             return ans;
+//         }
+
+//         TreeMap<Integer, Integer> map = new TreeMap<>();
+//         Queue<Tuple> q = new LinkedList<>();
+
+//         q.offer(new Tuple(root, 0));
+
+//         while (!q.isEmpty()) {
+
+//             Tuple cur = q.poll();
+
+//             TreeNode node = cur.node;
+//             int hd = cur.hd;
+
+            
+//             map.put(hd, node.val);
+
+//             if (node.left != null) {
+//                 q.offer(new Tuple(node.left, hd+1));
+//             }
+
+//             if (node.right != null) {
+//                 q.offer(new Tuple(node.right, hd + 1));
+//             }
+//         }
+
+//         for (int value : map.values()) {
+//             ans.add(value);
+//         }
+
+//         return ans;
 
 
